69285f37bac31c5ef8bec7053795587d51ebb70f
Mona Lisa <joannagrosshans2@gmail.com>
2020-09-20 14:21:39 -0500

    js added task item

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.classNAme = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+buttonEl.addEventListener("click", createTaskHandler);
+
+
